bdf18d376e1b3fb953103eb645e45db15c409a5f
Pherlita <pherlita@gmail.com>
2025-09-25 20:05:04 +0200

    Init project

---
+++ Java_HelloWorld/src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		
+		System.out.println("5+3=" + 8);
+		
+		
+
+	}
+
+}
